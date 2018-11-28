ba306d59a88535caea41a3849b976789c707d9cf
PhilPalmer <phil.palmer.email@gmail.com>
2018-11-28 09:46:34 +0000

    Add regex check for ch value

--- bin/noReads.py
+++ bin/noReads.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+import re
 
 isochores = []
 
@@ -27,10 +28,11 @@ for i in range(0, len(isochores)):
 	if ( isochores[i][3] != "gap" ):
 		counter = 0
 		for j in range(0, len(reads)):
-			if( reads[j][8].endswith( "ch01" ) ): #suffix should correspond to correct chromosome input in line 5.
+			ending = re.search('(ch[0-9]+)', sys.argv[1]).group(0)
+			if( reads[j][8].endswith( ending ) ): #suffix should correspond to correct chromosome input in line 5.
 				if  float(reads[j][9]) <  float(isochores[i][0]) and (float(reads[j][9]) + float(reads[j][6])  > float(isochores[i][0])) and   ( ( float(reads[j][9]) + float(reads[j][6]) ) -  float(isochores[i][0]) ) > ( float(reads[j][9]) + float(reads[j][6]) )/2:
 					counter = counter +1
-				elif	float(reads[j][9]) > float(isochores[i][0]) and ( ( float(reads[j][9]) +  float(reads[j][6]) ) < float(isochores[i][1]) ):
+				elif float(reads[j][9]) > float(isochores[i][0]) and ( ( float(reads[j][9]) +  float(reads[j][6]) ) < float(isochores[i][1]) ):
 					counter = counter +1
 				elif float(reads[j][9]) < float(isochores[i][1]) and (float(reads[j][9]) + float(reads[j][6]) )> isochores[i][1] and float(isochores[i][1]) - float(reads[j][9]) > ( float(reads[j][9]) + float(reads[j][6]) )/2:
 					counter=counter+1
