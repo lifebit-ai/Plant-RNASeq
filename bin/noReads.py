2267ef06f4e22116efad95ebab0cdfd4259c3fdf
Pablo Prieto <pablo.prieto.barja@gmail.com>
2019-01-25 03:23:44 +0000

    Changes in the csv tables reading and writing

--- bin/noReads.py
+++ bin/noReads.py
@@ -20,7 +20,6 @@ seq2 = inputList2.splitlines()
 for i in range(1, len(seq2)):
 	reads.append ( seq2[i].split() )
 
-
 counter = 0
 
 no = 0;
@@ -39,8 +38,8 @@ for i in range(0, len(isochores)):
 					counter = counter +1
 				elif float(reads[j][9]) > float(isochores[i][0]) and ( ( float(reads[j][9]) +  float(reads[j][6]) ) < float(isochores[i][1]) ):
 					counter = counter +1
-				elif float(reads[j][9]) < float(isochores[i][1]) and (float(reads[j][9]) + float(reads[j][6]) )> isochores[i][1] and float(isochores[i][1]) - float(reads[j][9]) > ( float(reads[j][9]) + float(reads[j][6]) )/2:
+				elif float(reads[j][9]) < float(isochores[i][1]) and (float(reads[j][9]) + float(reads[j][6]) )> float(isochores[i][1]) and float(isochores[i][1]) - float(reads[j][9]) > ( float(reads[j][9]) + float(reads[j][6]) )/2:
 					counter=counter+1
 
 
-	print( i, counter )
+		print( counter )
